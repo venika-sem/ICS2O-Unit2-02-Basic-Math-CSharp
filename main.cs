a8aa06bdde49484bc28fb0d326277a9a5aeec640
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-23 17:22:22 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,19 @@
+// Created by: Venika Sem
+// Created on: Mar 2022
+//
+// This program calculates the perimeter and area
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 5 cm and 3 cm.");
+        Console.WriteLine("The area is: " + (5 * 3) + " cm².");
+        Console.WriteLine("The perimeter is: " + (2 * (5 + 3)) + " cm.");
+
+        Console.WriteLine("\nDone.");
+    }
 }
